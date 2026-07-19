472d6a832a44f96f55070df1a22fa333ab3caf24
Mikhail Volohatyi <m1k4dev@yandex.ru>
2026-07-19 17:29:07 +0700

    feat: initialize project with CLI menu

---
+++ main.py
@@ -0,0 +1,31 @@
+# Консольное приложение - Менеджер задач
+# Функционал приложения:
+# Добавление
+# Удаление
+# Редактирование
+# Получение списка
+# Добавление / удаление тэгов
+
+def main():
+    print("Task menager. help - для справки.")
+    while True:
+        raw = input("> ").strip()
+        parts = raw.split()
+        cmd, args = parts[0], parts[1:]
+        match cmd:
+            case "help":
+                pass
+            case "add":
+                pass
+            case "remove":
+                pass
+            case "edit":
+                pass
+            case "tags":
+                pass
+            case "exit":
+                break
+            case _:
+                print("Не известная команда")
+        
+main()
